9f16eefc76c042ead3512f9daf61a9cb3cf17bed
ananthvk <mailstoanantha@gmail.com>
2025-04-12 17:06:41 +0530

    add repl and script mode of execution

--- src/python_lox/__main__.py
+++ src/python_lox/__main__.py
@@ -1,2 +1,41 @@
-def main():
-    print('hello world')
+import typer
+from rich import print
+from .lox import Lox
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+@app.command("")
+def main(file: Annotated[str, typer.Argument(help="Run this script")] = ""):
+    """
+    Run FILE in script mode if FILE is provided. Otherwise run in interactive mode
+    """
+
+    lox = Lox()
+
+    # Run in script mode
+    if file:
+        source = ""
+        with open(file, "r") as f:
+            source = f.read()
+        return lox.run(source)
+
+    # Run in REPL mode
+    print(f"[bold]Pylox {lox.version} ({lox.build_date})")
+    print(f'Type [bold]"exit"[/bold] to exit the interpeter')
+
+    while True:
+        print(f"[blue]>>> ", end="")
+        line = input()
+        if line == "exit":
+            break
+        lox.run(line)
+
+
+def run():
+    app()
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/python_lox/lox.py
@@ -0,0 +1,10 @@
+class Lox:
+    version = '0.1.0'
+    build_date = '12 April 2025 17:01'
+
+    def run(self, source: str) -> None:
+        """
+        Execute the source program
+        """
+        print('RUN')
+        return 0
